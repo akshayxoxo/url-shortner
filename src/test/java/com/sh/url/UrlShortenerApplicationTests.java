7403c4526300f187c9225a11e2694255cf17d0ed
Akshay <Akshay@DESKTOP-I1A46O4>
2020-07-01 18:34:40 +0530

    Initialized Spring boot project

---
+++ src/test/java/com/sh/url/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
